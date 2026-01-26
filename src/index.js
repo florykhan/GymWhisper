50888731e327a02cc8084c6ed7a3cf33dd2e68bd
florykhan <florykhan@gmail.com>
2026-01-25 20:43:37 -0800

    update configuration while troubleshooting GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import { LanguageProvider } from './contexts/LanguageContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <HashRouter basename="/GymWhisper">
     <LanguageProvider>
       <App />
     </LanguageProvider>
